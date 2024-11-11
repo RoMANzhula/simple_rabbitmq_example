4d08b80f62b05aa38690dfdbca1c0d932b7d8cdd
RoMANzhula <roman1985al@ukr.net>
2024-11-11 20:23:47 +0200

    Up MessageSenderService - producer-modul - add mappers for different queue senders

--- rabbitmq-producer/src/main/java/org/romanzhula/rabbitmqproducer/services/MessageSenderService.java
+++ rabbitmq-producer/src/main/java/org/romanzhula/rabbitmqproducer/services/MessageSenderService.java
@@ -18,16 +18,47 @@ public class MessageSenderService {
     @Value("${rabbitmq.queue.name.one}")
     private String queueNameOne;
 
+    @Value("${rabbitmq.queue.name.two}")
+    private String queueNameTwo;
+
     private final AmqpTemplate amqpTemplate;
 
-    public void sendMessage(String message) {
+    public void sendMessageFirstQueue(String message) {
         if (message.isBlank()) {
-            log.error("Text cannot be empty!");
-            throw new InvalidTextException("Text cannot be empty!");
+            log.error("First queue - text cannot be empty!");
+            throw new InvalidTextException("First queue - text cannot be empty!");
         }
 
-        log.info("Text ({}) sent successfully.", message);
+        log.info("First queue - text ({}) sent successfully.", message);
         amqpTemplate.convertAndSend(queueNameOne, message);
+    }
+
+    public void sendMessageSecondQueue(String message) {
+        if (message.isBlank()) {
+            log.error("Second queue - text cannot be empty!");
+            throw new InvalidTextException("Second queue - text cannot be empty!");
+        }
+
+        log.info("Second queue - text ({}) sent successfully.", message);
+        amqpTemplate.convertAndSend(queueNameTwo, message);
+    }
+
+    public void sendMessageManuallyInputQueue(
+            String message,
+            String queueNameManually
+    ) {
+        if (message.isBlank()) {
+            log.error("Manually queue - text cannot be empty!");
+            throw new InvalidTextException("Manually queue - text cannot be empty!");
+        }
+
+        if (queueNameManually.isBlank()) {
+            log.error("Manually queue - queue name cannot be empty!");
+            throw new InvalidTextException("Manually queue - queue name cannot be empty!");
+        }
+
+        log.info("Manually queue - text ({}) sent successfully.", message);
+        amqpTemplate.convertAndSend(queueNameManually, message);
     }
 
 }
