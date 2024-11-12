5c276e2546d7b92add0840d7862781398d159e4a
RoMANzhula <roman1985al@ukr.net>
2024-11-12 23:10:58 +0200

    Add RabbitmqSecondConsumer service - second-consumer-modul - example two consumers for one queue (read messages from the queue one by one)

---
+++ rabbitmq-second-consumer/src/main/java/org/romanzhula/rabbitmqsecondconsumer/services/RabbitmqSecondConsumer.java
@@ -0,0 +1,27 @@
+package org.romanzhula.rabbitmqsecondconsumer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.romanzhula.rabbitmqsecondconsumer.configuration.RabbitmqSecondConsumerConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RabbitmqSecondConsumer {
+
+    private final RabbitmqSecondConsumerConfig rabbitmqSecondConsumerConfig;
+
+
+    @RabbitListener(queues = "#{@rabbitmqSecondConsumerConfig.getQueueNameTwo()}", containerFactory = "rabbitListenerContainerFactory")
+    public void receiveQueueTwo(String text) {
+        getLogQueueMessage(rabbitmqSecondConsumerConfig.getQueueNameTwo(), text);
+    }
+
+    private void getLogQueueMessage(String queueName, String text) {
+        log.info("Text from queue {}: {}", queueName, text);
+    }
+
+}
