fa03cb8b1eeaa45ebc47f42d444d71b7539511a0
RoMANzhula <roman1985al@ukr.net>
2024-11-12 18:22:59 +0200

    Up RabbitmqConsumer - consumer-modul - to using @Value annotation for queue names

--- rabbitmq-consumer/src/main/java/org/romanzhula/rabbitconsumer/services/RabbitmqConsumer.java
+++ rabbitmq-consumer/src/main/java/org/romanzhula/rabbitconsumer/services/RabbitmqConsumer.java
@@ -1,22 +1,32 @@
 package org.romanzhula.rabbitconsumer.services;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.romanzhula.rabbitconsumer.configurations.RabbitmqConsumerConfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class RabbitmqConsumer {
 
-    @RabbitListener(queues = {"QueueForFistConsumerTransient"})
+    private final RabbitmqConsumerConfig rabbitmqConsumerConfig;
+
+
+    @RabbitListener(queues = "#{@rabbitmqConfigConsumer.getQueueNameOne()}")
     public void receiveQueueOne(String text) {
-        log.info("Text from queue QueueForFistConsumerTransient: {}", text);
+        getLogQueueMessage(rabbitmqConsumerConfig.getQueueNameOne(), text);
     }
 
-    @RabbitListener(queues = {"QueueForSecondConsumerDurable"})
+    @RabbitListener(queues = "#{@rabbitmqConfigConsumer.getQueueNameTwo()}")
     public void receiveQueueTwo(String text) {
-        log.info("Text from queue QueueForSecondConsumerDurable: {}", text);
+        getLogQueueMessage(rabbitmqConsumerConfig.getQueueNameTwo(), text);
+    }
+
+    private void getLogQueueMessage(String queueName, String text) {
+        log.info("Text from queue {}: {}", queueName, text);
     }
 
 }
